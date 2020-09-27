80de06f88f7ab8620d21378c6673862dce3f8130
Miki Masumoto <masumotomiki18@gmail.com>
2020-09-27 10:00:30 +0900

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Head from "next/head";
+import Hello from "../components/Hello";
+
+export default () => (
+  <div className="root">
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>TITLE</title>
+    </Head>
+    <style jsx>{`
+      .root {
+        font-family: sans-serif;
+        line-height: 1.33rem;
+        margin-top: 8vh;
+      }
+      @media (min-width: 600px) {
+        .root {
+          margin-left: 21vw;
+          margin-right: 21vw;
+        }
+      }
+    `}</style>
+
+    <Hello />
+  </div>
+);
